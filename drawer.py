6a16e808703e565686a0a073d33f345bb837cf5e
Kathy Lau <imobilep501@gmail.com>
2016-12-31 16:35:30 -0500

    draw square on click!

--- drawer.py
+++ drawer.py
@@ -14,19 +14,24 @@ i = 1
 rect = pg.Rect(50, 50, 50, 50)
 basefont = pg.font.SysFont('Monospaced', 100)
 text = basefont.render('hello', 1, (150, 50, 60))
+fxnlist = []
 while(1):
+    screen.fill(pg.Color('white'))
+    screen.blit(text, (400, 400))
     for event in pg.event.get():
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 exit(0)
         elif event.type == pg.MOUSEBUTTONDOWN:
-            exit(0)
+            x,y = pg.mouse.get_pos()
+            fxnlist.append([x, y])
+            #draw.rect(screen,pg.Color('red'),(200,150,100,50))
+            #exit(0)
     if pg.key.get_pressed()[pg.K_e]:  # alt way if multiple keys held
         print 'e down'
-    rect.w += 1
-    rect.h += 1
-    screen.fill(pg.Color('white'))
-    screen.blit(text, (400, 400))
-    draw.rect(screen, pg.Color('red'), rect, 0)
+    rect.center = pg.mouse.get_pos()
+    draw.rect(screen,pg.Color('red'),rect, 0)
+    for fxn in fxnlist:
+        draw.rect(screen,pg.Color('red'),(fxn[0],fxn[1],50,50))
     pg.display.update()
     clock.tick(60)
